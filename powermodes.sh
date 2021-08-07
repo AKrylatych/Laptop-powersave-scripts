d49feb2ad156cada9eef7a694702a3ffb4523846
Hellerlight <77800527+Hellerlight@users.noreply.github.com>
2021-08-07 15:05:04 +0300

    1.0

--- powermodes.sh
+++ powermodes.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
+
 # This script is made to easily switch between performance and power economy modes with the terminal.
-# Creator: Hellerlight
-### Main code
-powermode() {
+# !!!! REQUIRES CPUFREQ TO WORK !!!!
+
+# Other recommended packages: 
+# powertop (to monitor current battery drain, cpu frequencies)
+# TLP (another battery economy package)
+
+###
+if [ $# = 0 ]; then # If no parameters are supplied, ask for them
     echo "Select your desired mode"
     echo " "
     echo "pwrs"
     echo "perf"
     read powermode
-    if [ $powermode = "pwrs" ]; then
-        echo "Changing to power saving mode"
+else
+    powermode=$1
+fi
+
+if [ $powermode = "pwrs" ]; then
+    echo "Changing to power saving mode"
+    for i in 0 1 2 3 4 5 6 7
+    do
+        echo "core $i: "
+        echo powersave > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor     # Sets the new scaling governor
+        cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor  # Prints the new scaling governor, easier to track if code failed
+    done 
+    elif [ $powermode = "perf" ]; then    # Same code, but for performance mode
+        echo "Changing to performance mode"    
         for i in 0 1 2 3 4 5 6 7
         do
             echo "core $i: "
-            echo powersave > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor 
-            cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor       
-        done 
-        elif [ $powermode = "perf" ]; then    
-            echo "Changing to performance mode"    
-            for i in 0 1 2 3 4 5 6 7
-            do
-                echo "core $i: "
-                echo performance > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor   
-                cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor        
-            done    
-        else
-            echo "wrong input"
-    fi
-}
-powermode
+            echo performance > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor   
+            cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor        
+        done    
+    else
+        echo "wrong input" # If input is wrong, nothing is changed
+fi
