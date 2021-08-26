18d71503b285bdecbcb60d5160ec2fca537e9c59
Hellerlight <77800527+Hellerlight@users.noreply.github.com>
2021-08-26 20:59:30 +0300

    Update
    Added optional colors to text
    Refactored most echo commands to printf
    Added a function to count how many threads the script needs to affect
    Added a WIP options menu
    Put everything into functions

--- powermodes.sh
+++ powermodes.sh
@@ -1,58 +1,97 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script is made to easily switch between performance and power economy modes with the terminal.
-# !!!! REQUIRES CPUFREQ TO WORK !!!!
+# REQUIRES CPUFREQ TO WORK 
 
 # Other recommended packages: 
+
 # powertop (to monitor current battery drain, cpu frequencies)
 # TLP (another battery economy package)
-###
 
-clear
-sudo bash -c 'echo "passive" > /sys/devices/system/cpu/intel_pstate/status'
-if [ $# = 0 ]; then # If no parameters are supplied, ask for them
-    echo ""
-    echo "Current power governor: $( cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor )"
-    echo "Select your desired CPU usagemode"
-    echo ""
-    echo "pwrs  - Triggers power saving mode"
-    echo "perf  - Triggers performance mode"
-    echo "std   - Triggers gradual scaling mode"
-    read powermode  
-    echo ""
-else
-    powermode=$1
-fi
+## Functions
+init() {
+    # Defines used colors
+    YELLOW='\033[0;33m'
+    NC='\033[0m'    
 
-case $powermode in
-    pwrs)
-            echo "Changing to power saving mode..."
-            for i in 0 1 2 3 4 5 6 7
-            do
-                echo "core $i: "
-                echo powersave > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor     # Sets the new scaling governor
-                cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor  # Prints the new scaling governor, easier to track if code failed              
-            done   
-            ;;
-    perf)
-            echo "Changing to performance mode..."
-            for i in 0 1 2 3 4 5 6 7
-            do
-                echo "core $i: "
-                echo performance > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor     # Sets the new scaling governor
-                cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor  # Prints the new scaling governor, easier to track if code failed
-            done 
-            ;;
-    std)
-            echo "Changing to gradual scaling mode..."
-            for i in 0 1 2 3 4 5 6 7
-            do
-                echo "core $i: "
-                echo ondemand > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor     # Sets the new scaling governor
-                cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor  # Prints the new scaling governor, easier to track if code failed
-            done 
-            ;;   
-    *)
-            echo "wrong input"
-            ;;
-esac
+    # Disables pstate driver to enable other governors
+    if [ /sys/devices/cpu/intel_pstate/status != "passive" ]; then  
+        sudo bash -c 'echo "passive" > /sys/devices/system/cpu/intel_pstate/status' 
+    fi
+
+    # Finds the number of Threads
+    THREADCOUNT=($( echo /sys/devices/system/cpu/cpu[0-99] )) 
+    THREADCOUNT="${#THREADCOUNT[@]}"
+    printf "Threads located: $THREADCOUNT\n"    
+}
+
+menustart() {
+    clear
+        printf "|-----------------------------$CURRMENU------------------------------------|\n"
+}
+
+main() {
+    # If no parameters are supplied, ask for them
+    if [ $# = 0 ]; then        
+        CURRMENU="Main Menu"
+        menustart 
+        printf "Current power governor: ${YELLOW}$( cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor )${NC}\n\n"
+        printf "Select your desired CPU usagemode\n"
+        printf "${YELLOW}pwrs${NC}  - Enables power saving mode\n"
+        printf "${YELLOW}perf${NC}  - Enables performance mode\n"
+        printf "${YELLOW}std${NC}   - Enables gradual scaling mode\n"
+        printf "${YELLOW}opt${NC}   - Opens the option menu\n"
+        read powermode
+    elif [ $1 = "startup" ]; then
+        printf "bonk"
+    else
+        powermode=$1
+    fi
+
+    # Changes the input to the governor name
+    case $powermode in 
+        pwrs)
+                powermode="powersave"
+                modechange 
+                ;;
+        perf)
+                powermode="performance"
+                modechange
+                ;;
+        std)
+                powermode="ondemand"
+                modechange
+                ;;   
+        opt)
+                options
+                ;;
+        *)
+                printf "Incorrect input"
+                ;;
+    esac
+}
+
+options() {
+    CURRMENU="Options"
+    menustart
+    printf "beans[y/n]\n"
+    read lol
+    main
+}
+
+modechange() {        
+    # Changes the power governor to the specified value
+    printf "Current selected mode:\n"$powermode"\n" 
+        for i in `seq 0 $( expr $THREADCOUNT - 1 )`
+        do
+            printf "core $i: "
+            echo $powermode > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor     
+            printf "${YELLOW} $( cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor )${NC}\n"
+        done 
+}
+
+### Main code
+init
+main
+
+exit 0 # Signal sucessful execution
